33247d8c95255cfe35cbc38e94601ff7e4210535
putzjuau <joaovictorfaisca@gmail.com>
2024-04-29 15:50:39 -0300

    feat: create User  entity

---
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,32 @@
+package com.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String LastName;
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType usersType;
+}
